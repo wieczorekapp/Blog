8954e8038111e00cdace39b318b338f097accb74
wieczorekapp <61647394+wieczorekapp@users.noreply.github.com>
2022-09-06 21:50:46 +0200

    On Delet behavior on Posts

--- Data.Layer/Configurations/PostConfiguration.cs
+++ Data.Layer/Configurations/PostConfiguration.cs
@@ -38,12 +38,19 @@ namespace Blog.DataLayer.Configurations
             builder
                 .HasOne(x => x.User)
                 .WithMany(x => x.Posts)
-                .HasForeignKey(x => x.UserId);
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Restrict); // wyjatek, brak usuniecia postow
 
             builder
                 .HasOne(x => x.ApprovedBy)
                 .WithMany(x => x.PostsApproved)
                 .HasForeignKey(x => x.ApprovedByUserId);
+
+            builder
+                .HasOne(x => x.Category)
+                .WithMany(x => x.Posts)
+                .HasForeignKey(x => x.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
 
             builder
                 .HasMany(x => x.Tags)
---
+++ Data.Layer/Migrations/20220906194516_SetOnDeletToPosts.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.DataLayer.Migrations
+{
+    public partial class SetOnDeletToPosts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Categories_CategoryId",
+                table: "Posts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Users_UserId",
+                table: "Posts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Categories_CategoryId",
+                table: "Posts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Users_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Categories_CategoryId",
+                table: "Posts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_Users_UserId",
+                table: "Posts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Categories_CategoryId",
+                table: "Posts",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_Users_UserId",
+                table: "Posts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
